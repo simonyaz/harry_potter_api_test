e0c69a366032b7a51cf3d60bc1baa6e67ffb1c78
Simony de Almeida Azevedo <69819910+simonyaz@users.noreply.github.com>
2020-08-20 18:14:05 -0300

    novo expect
    Inclusao novo expect

--- cypress/integration/api_test.spec.js
+++ cypress/integration/api_test.spec.js
@@ -1,7 +1,8 @@
 describe('API Test of the Harry Potter API', () => {
 
     it('Returns a random Hogwarts House', () => {
-        cy.request({
+        cy.request(
+        {
             method: 'GET',
             url: 'sortingHat'
         }).then((response)=> {
@@ -14,17 +15,23 @@ describe('API Test of the Harry Potter API', () => {
 
     it('Returns all characters', () => {
        
-        cy.request({
+        cy.request(
+        {
             method: 'GET',
             url: 'characters',
             failOnStatusCode: false,
-            headers:
-            {
-                'key': '$2a$10$6q0t4VjTZh9Gqy7C6vhxluy4irLNKTMEByFdVgKpjeJrkotk1I22O',
+          //  APIkey:'$2a$10$6q0t4VjTZh9Gqy7C6vhxluy4irLNKTMEByFdVgKpjeJrkotk1I22O',
+            
+            
+           // headers:
+          //  {
+          //      'key': '$2a$10$6q0t4VjTZh9Gqy7C6vhxluy4irLNKTMEByFdVgKpjeJrkotk1I22O'
                 //'Content-Type': 'text/plain'
-            }
+          //  }
         }).then((response) => {
              expect(response.status).to.eq(409)
+             expect(response).property('body').to.contain({ 
+                error: 'Must pass API key for request',})
         })
 
 
