9cf7783c94aecc9d815db23a0dcd0b9cf080fa81
Simony de Almeida Azevedo <69819910+simonyaz@users.noreply.github.com>
2020-08-20 17:33:57 -0300

    implementando
    continuando a implementacao

--- cypress/integration/api_test.spec.js
+++ cypress/integration/api_test.spec.js
@@ -1,8 +1,33 @@
 describe('API Test of the Harry Potter API', () => {
-    it('GET - read', () => {
-        cy.request('GET', 'https://www.potterapi.com/v1/sortingHat').then((response)=> {
+
+    it('Returns a random Hogwarts House', () => {
+        cy.request({
+            method: 'GET',
+            url: 'sortingHat'
+        }).then((response)=> {
             expect(response).to.have.property('status',200)
+            expect(response.body).to.not.be.null
         })
+    })
+
+    
+
+    it('Returns all characters', () => {
+       
+        cy.request({
+            method: 'GET',
+            url: 'characters',
+            failOnStatusCode: false,
+            headers:
+            {
+                'key': '$2a$10$6q0t4VjTZh9Gqy7C6vhxluy4irLNKTMEByFdVgKpjeJrkotk1I22O',
+                //'Content-Type': 'text/plain'
+            }
+        }).then((response) => {
+             expect(response.status).to.eq(409)
+        })
+
+
     })
 })
 
