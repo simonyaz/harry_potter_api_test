b8d01b063b0b9c100732ef426fb37e13b26bbf47
Simony de Almeida Azevedo <69819910+simonyaz@users.noreply.github.com>
2020-08-21 00:00:34 -0300

    Update api_test_spec.js

--- cypress/integration/api_test.spec.js
+++ cypress/integration/api_test.spec.js
@@ -12,29 +12,37 @@ describe('API Test of the Harry Potter API', () => {
     })
 
     
-
-    it('Returns all characters', () => {
+    //Wihout passing a api key
+    it('Should returns all characters, error - without authentication', () => {
        
         cy.request(
         {
             method: 'GET',
             url: 'characters',
-            failOnStatusCode: false,
-          //  APIkey:'$2a$10$6q0t4VjTZh9Gqy7C6vhxluy4irLNKTMEByFdVgKpjeJrkotk1I22O',
-            
-            
-           // headers:
-          //  {
-          //      'key': '$2a$10$6q0t4VjTZh9Gqy7C6vhxluy4irLNKTMEByFdVgKpjeJrkotk1I22O'
-                //'Content-Type': 'text/plain'
-          //  }
+            failOnStatusCode: false,          
         }).then((response) => {
              expect(response.status).to.eq(409)
              expect(response).property('body').to.contain({ 
                 error: 'Must pass API key for request',})
         })
+    })
 
+     //Passing a api key
+     it('Returns all characters, success authentication', () => { 
+       
+        cy.request(
+        {
+            method: 'GET',
+            url: 'characters?key=$2a$10$6q0t4VjTZh9Gqy7C6vhxluy4irLNKTMEByFdVgKpjeJrkotk1I22O',        
+        }).as('getCharacters')
+        .then((response) => {
 
+            
+            expect(response.status).to.eq(200)
+            expect(response.body).to.have.length(195)
+          //  expect(response.body).has.property('name', 'Hermione Granger');             
+                
+        })
     })
 })
 
